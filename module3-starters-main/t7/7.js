ae50b6c50c4e313a7ba4cf7d4c48b28b8117b472
Paupeli <pauliina.helle@metropolia.fi>
2025-04-16 19:12:13 +0300

    JavaScript moduuli 3 tehtävät 1, 2, 5 ja 7.

---
+++ module3-starters-main/t7/7.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const trigger = document.getElementById('trigger');
+const target = document.getElementById('target');
+
+trigger.addEventListener('mouseover', function() {
+  target.src = 'picB.jpg';
+});
+
+trigger.addEventListener('mouseout', function() {
+  target.src = 'picA.jpg';
+});
+
+
